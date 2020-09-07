161488f8162cf613d87556f7a5578f4d160edd86
easunarunachalam <45492958+hekstralabarunachalam@users.noreply.github.com>
2020-09-07 10:55:48 -0400

    updated batch image conversion

--- util.py
+++ util.py
@@ -282,8 +282,11 @@ def sdt_to_images(
         illprof=1,
         intensity_scale=100,
         tau_scale=10,
+        return_images=False,
         return_objects=False,
-        only_first_n=None
+        return_all_fns=False,
+        only_first_n=None,
+        regenerate=False
     ):
     '''
     generate intensity images from all sdt files
@@ -305,53 +308,74 @@ def sdt_to_images(
 
     '''
 
-    sdt_filenames = [fn for fn in glob.glob(os.path.join(exp_folder,"**/*.sdt"), recursive=True) if ((exclude_folder not in fn) and (exclude_name not in fn))]
+    # get list of sdt files to process
+    
+    sdt_filenames_all = sorted([fn for fn in Path(exp_folder).glob("**/*.sdt") if fn.is_file()])
 
-    timeseries    = [None]*len(sdt_filenames)
-    sdt_list      = [None]*len(sdt_filenames)
+    if not regenerate:
+        sdt_filenames_use = []
+
+        for sdt_fn in sdt_filenames_all:
+            img_fn = Path.joinpath(sdt_fn.parent, sdt_fn.stem + "_intensity_corr.tiff")
+            if not img_fn.is_file():
+                sdt_filenames_use.append(sdt_fn)
+    else:
+        sdt_filenames_use = sdt_filenames_all
+
+        
+        
+    # image generation
+        
+    timeseries    = [None]*len(sdt_filenames_use)
+    sdt_list      = [None]*len(sdt_filenames_use)
 
     clearline = 120*" "
 
-    t0 = time.time()
-    for i, fn in enumerate(sdt_filenames):
+    if len(sdt_filenames_use) > 0:
+        t0 = time.time()
+        for i, sdt_fn in enumerate(sdt_filenames_use):
+
+            t = time.time() - t0
+            if i == 0:
+                est_time = 0
+            else:
+                est_time = t*len(sdt_filenames_use)/(i)
+            print("file {:d}/{:d} | elapsed time = {:3.2f} / {:3.2f} (est.) | current file: {:s} ".format(
+                    i+1, len(sdt_filenames_use), t, est_time, str(sdt_fn)
+                ) + clearline, end="\r")
+
+            sdt = SDT(sdt_fn)
+            im = sdt.image(
+                        rawsum=im_save,
+                        corrsum=im_save,
+                        meantau=False,
+                        adjust_intensity=True,
+                        illprof=illprof,
+                        intensity_scale=intensity_scale,
+                        tau_scale=tau_scale
+                    )
+
+            timeseries[i] = im
+
+            if return_objects:
+                sdt_list[i] = sdt
+
+            if only_first_n and (i+1)>=only_first_n:
+                break
 
         t = time.time() - t0
-        if i == 0:
-            est_time = 0
-        else:
-            est_time = t*len(sdt_filenames)/(i)
-        print("file {:d}/{:d} | elapsed time = {:3.2f} / {:3.2f} (est.) | current file: {:s} ".format(
-                i+1, len(sdt_filenames), t, est_time, fn
-            ) + clearline, end="\r")
+        print(clearline + "\r{:d} files converted | elapsed time = {:3.2f}".format(i+1, t) + clearline, end="\n")
 
-        sdt = SDT(fn)
-        im = sdt.image(
-                    rawsum=im_save,
-                    corrsum=im_save,
-                    meantau=False,
-                    adjust_intensity=True,
-                    illprof=illprof,
-                    intensity_scale=intensity_scale,
-                    tau_scale=tau_scale
-                )
-
-        timeseries[i] = im
-
-        source_folder, filename = fn.rsplit(os.path.sep, maxsplit=1)
-
-        if return_objects:
-            sdt_list[i] = sdt
-
-        if only_first_n and (i+1)>=only_first_n:
-            break
-
-    t = time.time() - t0
-    print(clearline + "\r{:d} files converted | elapsed time = {:3.2f}".format(i+1, t) + clearline, end="\n")
-
-    if return_objects:
-        return sdt_filenames, timeseries, sdt_list
+    if return_images and return_objects:
+        return sdt_filenames_use, timeseries, sdt_list
+    elif return_images:
+        return sdt_filenames_use, timeseries
+    elif return_objects:
+        return sdt_filenames_use, sdt_list
+    elif return_all_fns:
+        return sdt_filenames_all
     else:
-        return sdt_filenames, timeseries
+        return sdt_filenames_use
 
 class decay_group:
 
@@ -472,7 +496,8 @@ class decay_group:
             for key in self.params.keys():
                 self.params[str(key)].value = self.final_vals[str(key)]
 
-            out = m.minimize(method="emcee", burn=2000, steps=3000)
+            out = m.minimize(method="emcee", burn=500, steps=1000)
+            # out = m.minimize(method="emcee", burn=2000, steps=3000)
 
         else:
             out = m.minimize(method=method)
