36692aabd6081acfc7a025229d9decfdfe18e5c7
easunarunachalam <45492958+hekstralabarunachalam@users.noreply.github.com>
2020-09-08 15:18:24 -0400

    moved colorbar to utils

--- util.py
+++ util.py
@@ -270,3 +270,14 @@ class SDT(object):
             return np.mean(cell_px), np.sum(cell_px), cell_px
         else:
             return np.mean(cell_px), np.sum(cell_px)
+
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+# for creating appropriately-sized colorbars for subplots
+def colorbar(mappable):
+    ax = mappable.axes
+    fig = ax.figure
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    return fig.colorbar(mappable, cax=cax)
